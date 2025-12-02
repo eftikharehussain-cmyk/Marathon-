72f07fcb1319f7875d767a1d756f0ce6c1c85814
Mohammad hussain <eftikharehussain@gmail.com>
2025-12-02 14:19:54 +0430

    Add Marathon counting script in README.py
    Add Python script to count values less than given inputs.

---
+++ README.py
@@ -0,0 +1,12 @@
+# Marathon-
+t = int(input())
+for _ in range(t):
+	a, b, c, d = map(int, input().split())
+	count = 0
+	if a < b:
+		count += 1
+	if a < c:
+		count += 1
+	if a < d:
+		count += 1
+	print(count)
